01f96a803032519060240591874b445ff2300a02
Tanzir Razzaque <tanzir.razzaque@gmail.com>
2025-07-23 00:50:11 +0600

    Extract text for both eng and ben

--- text_extract.py
+++ text_extract.py
@@ -3,9 +3,7 @@ from PIL import ImageOps, ImageFilter
 import pytesseract
 import os
 
-
-
-def ocr_bangla_pdf(pdf_path, lang='ben'):
+def ocr_pdf(pdf_path, lang='ben+eng'):
     custom_config = "--oem 1 --psm 3"
     images = convert_from_path(pdf_path)
     full_text = ""
@@ -18,11 +16,11 @@ def ocr_bangla_pdf(pdf_path, lang='ben'):
 
 # Run
 pdf_path = "bangla-text.pdf"
-extracted_text = ocr_bangla_pdf(pdf_path)
+extracted_text = ocr_pdf(pdf_path)
 
-with open("bangla_ocr_output.txt", "w", encoding="utf-8") as f:
+with open("ocr_output.txt", "w", encoding="utf-8") as f:
     f.write(extracted_text)
 
-print("✅ OCR extraction complete.")
+print("OCR extraction complete.")
 
 
